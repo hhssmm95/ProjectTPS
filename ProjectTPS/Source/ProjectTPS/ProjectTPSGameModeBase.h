e96e65c5c4a870ac6702ef8594278664b9656bb3
hhssmm95 <hhssmm95@gmail.com>
2021-05-12 20:53:42 +0900

    FirstCommit

---
+++ ProjectTPS/Source/ProjectTPS/ProjectTPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTPS_API AProjectTPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
